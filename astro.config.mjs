0f9b73d4b0e92ee40a32f4664246eabe05a09b11
= <=>
2023-09-14 21:49:53 +0700

    add blog page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
+import { remarkReadingTime } from 'remake-reading-time.mjs';
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(
-      {
-          applyBaseStyles: false,
-      }
-  )]
+    integrations: [tailwind(
+        {
+            applyBaseStyles: false,
+        }
+    )],
+
+    markdown: {
+        remarkPlugins: [remarkReadingTime]
+    }
+
 });
