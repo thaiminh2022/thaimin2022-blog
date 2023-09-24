05a954f257114a5b476c1d7daacb1d91bedaa19d
= <=>
2023-09-24 08:37:34 +0700

    Finish blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
-import { remarkReadingTime } from 'remake-reading-time.mjs';
-import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from "./remake-reading-time.mjs";
+import astroRemark from "@astrojs/markdown-remark";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(
-        {
+    integrations: [
+        tailwind({
             applyBaseStyles: false,
-        }
-    )],
+        }),
+    ],
 
     markdown: {
-        remarkPlugins: [remarkReadingTime]
-    }
-
+        remarkPlugins: [remarkReadingTime],
+    },
 });
